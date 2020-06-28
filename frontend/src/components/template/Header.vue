6a9a69acd3f19b291438551522a44c5ab9513e89
Vicente Jr <vicente.junior@live.com>
2020-06-27 23:31:53 -0300

    Layout
    Layout

---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <header class="header">
+      <a class="toggle" @click="toggleMenu" v-if="!hideToggle">
+          <i class="fa fa-lg" :class="icon"></i>
+      </a>
+      <h1 class="title">
+          {{ title }}
+      </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+      title: String,
+      hideToggle: Boolean 
+  },
+  computed:{
+      icon(){
+          return "fa-angle-left"
+      }
+  },
+  methods: {
+      toggleMenu(){
+
+      }
+  }
+};
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: linear-gradient(to right, #136a8a, #267871);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }   
+
+    .title{
+        font-size: 1.2rem;
+        color: white;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .title a{
+        color: white;
+        text-decoration: none;
+    }
+
+    header.header > a.toggle{
+        width: 60px;
+        height: 100%;
+        color: white;
+        justify-self: flex-start;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover{
+       background-color: rgba(0,0,0,0.1);
+    }
+</style>
